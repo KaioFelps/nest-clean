7a7ad3449ca1729125f81dd700fe249d3c5d7427
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-10-19 22:51:14 -0300

    feat: create question controller

---
+++ src/controllers/create-question-controller.ts
@@ -0,0 +1,44 @@
+import { Body, Controller, Post, UseGuards, UsePipes } from "@nestjs/common";
+import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
+import { PrismaService } from "src/prisma/prisma.service";
+import { CurrentUser } from "src/auth/current-user-decorator";
+import { userTokenPayload } from "src/auth/jwt.strategy";
+import { z } from "zod";
+import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
+
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+});
+
+type CreateQuestionBody = z.infer<typeof createQuestionBodySchema>;
+
+@Controller("/questions")
+@UseGuards(JwtAuthGuard)
+export class CreateQuestionController {
+  constructor(private prisma: PrismaService) {}
+
+  @Post()
+  async handle(
+    @Body(new ZodValidationPipe(createQuestionBodySchema))
+    body: CreateQuestionBody,
+    @CurrentUser() user: userTokenPayload,
+  ) {
+    const { title, content } = body;
+
+    await this.prisma.question.create({
+      data: {
+        authorId: user.sub,
+        title,
+        content,
+        slug: content
+          .toLowerCase()
+          .normalize("NFD")
+          .trimStart()
+          .trimEnd()
+          .replaceAll(" ", "_")
+          .replaceAll(/__+/g, ""),
+      },
+    });
+  }
+}
