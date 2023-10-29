8a8968a03ad0ed5072a8e1274a456ff5c77ae2a9
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-10-29 13:09:31 -0300

    feat: prisma question mapper

---
+++ src/infra/database/prisma/mappers/prisma-question-mapper.ts
@@ -0,0 +1,39 @@
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+import { Question } from "@/domain/forum/enterprise/entities/question";
+import { Slug } from "@/domain/forum/enterprise/entities/value-objects/slug";
+import { Prisma, Question as PrismaQuestion } from "@prisma/client";
+
+export class PrismaQuestionMapper {
+  static toDomain(prismaQuestion: PrismaQuestion): Question {
+    const domainQuestion = Question.create({
+      authorId: new UniqueEntityId(prismaQuestion.authorId),
+      content: prismaQuestion.content,
+      title: prismaQuestion.title,
+      createdAt: prismaQuestion.createdAt,
+      updatedAt: prismaQuestion.updatedAt,
+      bestAnswerId: prismaQuestion.bestAnswerId
+        ? new UniqueEntityId(prismaQuestion.bestAnswerId)
+        : null,
+      slug: Slug.create(prismaQuestion.slug),
+    });
+
+    return domainQuestion;
+  }
+
+  static toPrisma(
+    domainQuestion: Question,
+  ): Prisma.QuestionUncheckedCreateInput {
+    const prismaQuestion: Prisma.QuestionUncheckedCreateInput = {
+      id: domainQuestion.id.toString(),
+      authorId: domainQuestion.authorId.toString(),
+      bestAnswerId: domainQuestion.bestAnswerId?.toString(),
+      content: domainQuestion.content,
+      title: domainQuestion.title,
+      slug: domainQuestion.slug.value,
+      createdAt: domainQuestion.createdAt,
+      updatedAt: domainQuestion.updatedAt,
+    };
+
+    return prismaQuestion;
+  }
+}
---
+++ src/infra/database/prisma/mappers/readme.md
@@ -0,0 +1,4 @@
+# Mappers
+Os mappers são classes responsáveis por converter uma entidade no formato de uma camada para o formato de outra camada.
+
+ex: camada do banco de dados para a camada de use-cases (model para entidade)
