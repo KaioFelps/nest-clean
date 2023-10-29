e0bb7ca8d4a9910a6dd0b28739cca2f8e3cc7b16
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-10-29 17:40:24 -0300

    feat(student entity): add email and password fields

--- src/domain/forum/enterprise/entities/student.ts
+++ src/domain/forum/enterprise/entities/student.ts
@@ -3,11 +3,21 @@ import { UniqueEntityId } from "@/core/entities/unique-entity-id";
 
 interface IStudent {
   name: string;
+  email: string;
+  password: string;
 }
 
 export class Student extends Entity<IStudent> {
   get name() {
     return this.props.name;
+  }
+
+  get email() {
+    return this.props.email;
+  }
+
+  get password() {
+    return this.props.password;
   }
 
   static create(props: IStudent, id?: UniqueEntityId) {
